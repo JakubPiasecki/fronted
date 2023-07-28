7be042955f9b25b59ba418452a38c8d4a1cf80f9
Jakub Piasecki <jakubpiasecki@interia.pl>
2023-07-28 13:20:47 +0200

    refactor: Simplify employee form and add/edit logic

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -5,15 +5,20 @@ import { Employee } from '../employee';
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
-  styleUrls: ['./employee-detail.component.scss'],
+  styleUrls: ['./employee-detail.component.scss']
 })
 export class EmployeeDetailComponent implements OnInit, OnChanges {
   @Input() employee?: Employee;
-  @Output() employeeUpdated = new EventEmitter<Employee>();
+  @Input() employees: Employee[] = [];
+  @Output() employeesUpdated = new EventEmitter<Employee[]>();
   @Output() closed = new EventEmitter<void>();
   employeeForm!: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {}
+  isFormVisible = false;
+  availableSkills = ['skill1', 'skill2', 'skill3'];
+  updatedEmployees: Employee[] = [];
+  new = false;
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
     this.initForm();
@@ -22,6 +27,7 @@ export class EmployeeDetailComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['employee']) {
       this.initForm();
+      this.hideForm();
     }
   }
 
@@ -29,25 +35,77 @@ export class EmployeeDetailComponent implements OnInit, OnChanges {
     this.employeeForm = this.formBuilder.group({
       name: [this.employee?.name || '', Validators.required],
       surname: [this.employee?.surname || '', Validators.required],
-      skills: [this.employee?.skills || '', Validators.required],
+      skills: [this.employee?.skills || [], Validators.required],
       hireDate: [this.employee?.hireDate || '', Validators.required],
-      manager: [this.employee?.manager || '', Validators.required],
+      manager: [this.employee?.manager || '', Validators.required]
     });
   }
 
   onSubmit(): void {
-    if (this.employeeForm.valid && this.employee) {
+    if (!this.new) {
       const updatedEmployee: Employee = {
         ...this.employee,
-        ...this.employeeForm.value,
+        ...this.employeeForm.value
       };
-      this.employeeUpdated.emit(updatedEmployee);
+      const updatedEmployees = this.employees.map(emp =>
+        emp.id === updatedEmployee.id ? updatedEmployee : emp
+      );
+      this.employeesUpdated.emit(updatedEmployees);
     } else {
-      this.closed.emit();
+      const updatedEmployee: Employee = {
+        ...this.employee,
+        ...this.employeeForm.value
+      };
+      this.employeesUpdated.emit([...this.updatedEmployees,updatedEmployee]);
+      this.isFormVisible = false;
+      this.employee = undefined;
+      this.new = false;
     }
   }
 
+  onEdit(): void {
+    this.isFormVisible = true;
+  }
+
+  hideForm(): void {
+    this.isFormVisible = false;
+  }
+
   onClose(): void {
-    this.closed.emit();
+    this.hideForm();
+    this.employee = undefined;
+  }
+
+  onReset(): void {
+    if (this.employee) {
+      this.employeeForm.patchValue(this.employee);
+    } else {
+      this.employeeForm.patchValue({});
+    }
+  }
+
+
+  onAddEmployee(): void {
+    const newId = this.generateNewId();
+    const newEmployee: Employee = {
+      id: newId,
+      name: '',
+      surname: '',
+      skills: [],
+      hireDate: '',
+      manager: '',
+    };
+
+    this.updatedEmployees = [...this.employees];
+    this.employee = newEmployee;
+    this.new = true;
+    this.isFormVisible = true;
+    this.initForm();
+  }
+
+  private generateNewId(): string {
+    const maxIdNumber = this.employees.reduce((max, emp) => (Number(emp.id) > max ? Number(emp.id) : max), 0);
+    const newIdNumber = maxIdNumber + 1;
+    return newIdNumber.toString();
   }
 }
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -15,44 +15,15 @@ export class EmployeesComponent {
     this.selectedEmployee = employee;
   }
 
-  onEmployeeUpdated(updatedEmployee: Employee): void {
-    const index = this.employees.findIndex((emp) => emp.id === updatedEmployee.id);
-
-    if (index !== -1) {
-      this.employees[index] = updatedEmployee;
-      this.selectedEmployee = updatedEmployee;
-    }
-  }
-  onEmployeeDetailsClosed(): void {
-    if (
-      this.selectedEmployee &&
-      (!this.selectedEmployee.name || !this.selectedEmployee.surname || !this.selectedEmployee.hireDate)
-    ) {
-      const index = this.employees.findIndex((emp) => emp.id === this.selectedEmployee?.id);
-      if (index !== -1) {
-        this.employees.splice(index, 1);
-      }
-    }
+  onEmployeeUpdated(updatedEmployees: Employee[]): void {
+    this.employees = updatedEmployees;
     this.selectedEmployee = undefined;
   }
 
-  onAddEmployee(): void {
-    const newId = this.generateNewId();
-    const newEmployee: Employee = {
-      id: newId,
-      name: '',
-      surname: '',
-      skills: [],
-      hireDate: '',
-      manager: '',
-    };
-    this.employees.push(newEmployee);
-    this.selectedEmployee = newEmployee;
+  onEmployeeDetailsClosed(): void {
+    this.selectedEmployee = undefined;
   }
 
-  generateNewId(): string {
-    const maxIdNumber = this.employees.reduce((max, emp) => (Number(emp.id) > max ? Number(emp.id) : max), 0);
-    const newIdNumber = maxIdNumber + 1;
-    return newIdNumber.toString();
-  }
+
+
 }
