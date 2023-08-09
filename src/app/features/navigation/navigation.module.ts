dce9933c1730743c9432d74c0250537ca90e3b0f
Jakub Piasecki <jakubpiasecki@interia.pl>
2023-08-09 13:11:58 +0200

    Removed unnecessary imports from app.module

--- src/app/features/navigation/navigation.module.ts
+++ src/app/features/navigation/navigation.module.ts
@@ -4,16 +4,36 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { TranslateComponent } from './components/translate/translate.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { RouterLink } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [HeaderComponent, MessagesComponent, TranslateComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, TranslateModule, MatMenuModule, MatIconModule, MatButtonModule, MatBadgeModule, RouterLink],
+  imports: [
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    CommonModule,
+    TranslateModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    RouterLink,
+  ],
 })
 export class NavigationModule {}
